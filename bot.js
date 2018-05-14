ce0ca250557cc6200277471dea966be728a74494
Alex16857 <38953925+Alex16857@users.noreply.github.com>
2018-05-14 13:22:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,5 +23,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Start') {
+    	message.channel.send('**Hello and welcome to Gamerz. Here I hope you have a good time! First you must read the rules.**');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

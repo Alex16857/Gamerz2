fdcf04bb0c120779a1f3071781fa2b2f4e7ca410
Alex16857 <38953925+Alex16857@users.noreply.github.com>
2018-05-14 10:05:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,4 +18,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(NDQwODkzNTM0MDM2NDI2NzUy.DdODXQ.Ewe-Ol1wqS_n9FcjN0UuJoNO3kg);
+client.login(process.env.BOT_TOKEN);

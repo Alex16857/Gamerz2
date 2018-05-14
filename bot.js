0fb254806eb05d92932d01bcea27adb80378fe39
Alex16857 <38953925+Alex16857@users.noreply.github.com>
2018-05-14 14:27:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,18 +26,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === prefix + 'Start') {
-    	message.channel.send('**Hello and welcome to Gamerz. Here I hope you have a good time! First you must read the rules.
-#1 Don't spam outside of #spam
-#2 Don't advertise outside of #ads
-#3 Don't ask for roles
-#4 Only use the !rank command in #rank-checker
-#5 Don't be rude to @Owner @OwnerAssistant @Head Admin @Head Admin Assistant @Admin @Trail Admin @Pro Gamer @Head Mod @Mod @Trial Mod
-#6 Don't do bot commands outside of #bot-commands
-#7 Don't deathbattle outside of #arena
-#8 If you introduce yourself more than once you get a warning
-#9 Only introduce yourself in #introduce-yourself 
-#10 Please Introduce yourself so we can customize your roles
-#11 There is no NSFW. This is a gaming server. **');
+    	message.channel.send('**Hello and welcome to Gamerz. Here I hope you have a good time! First you must read the rules.\n#1 Don't spam outside of #spam\n#2 Don't advertise outside of #ads\n#3 Don't ask for roles\n#4 Only use the !rank command in #rank-checker\n#5 Don't be rude to @Owner @OwnerAssistant @Head Admin @Head Admin Assistant @Admin @Trail Admin @Pro Gamer @Head Mod @Mod @Trial Mod\n#6 Don't do bot commands outside of #bot-commands\n#7 Don't deathbattle outside of #arena\n#8 If you introduce yourself more than once you get a warning\n#9 Only introduce yourself in #introduce-yourself \n#10 Please Introduce yourself so we can customize your roles\n#11 There is no NSFW. This is a gaming server. **');
   	}
 });
 

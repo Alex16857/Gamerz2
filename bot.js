73642e43012920facec4c36c0fdc05bb83800420
Alex16857 <38953925+Alex16857@users.noreply.github.com>
2018-05-14 10:58:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.channel.send('***
-#1 Don't spam outside of #spam
-#2 Don't advertise outside of #ads
-#3 Don't ask for roles
-#4 Only use the !rank command in #rank-checker
-#5 Don't be rude to @Owner @Admin  @Pro Gamer @Mod 
-#6 Don't do bot commands outside of #bot-commands
-#7 Don't deathbattle outside of #arena
-#8 @everyone If you introduce yourself more than once you get a warning  
-#9 @everyone  only introduce yourself in #introduce-yourself 
-#10 Please Introduce yourself so we can customize your roles
-#11 There is no NSFW. This is a gaming server.
-~Arekkusu
-Server Owner
-***');
+    	message.channel.send('cool');
   	}
 });
 
